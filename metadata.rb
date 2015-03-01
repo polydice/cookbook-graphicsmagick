2d899bafd21c45d8a2b85f1d3b2be07aeb4ed371
Richard Lee <dlackty@gmail.com>
2015-03-01 23:59:24 +0800

    Update LICENSE

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'cookbook-graphicsmagick'
 maintainer       'YOUR_NAME'
 maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures cookbook-graphicsmagick'
 long_description 'Installs/Configures cookbook-graphicsmagick'
 version          '0.1.0'
