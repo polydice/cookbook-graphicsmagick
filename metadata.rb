a5f6ec137186e62dd4c1f3d0303d7cd2bc4dd907
frozenfung <frozenfung@gmail.com>
2015-03-04 15:30:55 +0800

    Publish package recipe

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,6 @@ license          'Apache 2.0'
 description      'Installs/Configures cookbook-graphicsmagick'
 long_description 'Installs/Configures cookbook-graphicsmagick'
 version          '0.1.0'
+
+
+depends "apt"
