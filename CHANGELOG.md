87ddbea53bb97b6ce5ff6dfff5e402f0f5f1f94e
Richard Lee <dlackty@gmail.com>
2015-03-08 01:11:06 +0800

    Bump cookbook version to 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.1
+
+* Add attributes to specify version and source code download URL
+
 # 0.1.0
 
-Initial release of cookbook-graphicsmagick
+* Initial release of cookbook-graphicsmagick
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'fung@polydice.com'
 license          'Apache 2.0'
 description      'Installs/Configures graphicsmagick'
 long_description 'Installs/Configures graphicsmagick'
-version          '0.1.0'
+version          '0.1.1'
 
 depends "apt"
